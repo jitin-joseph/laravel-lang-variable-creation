a5e159a9e908496cdf7617ae73431946ce9e7c16
jitin-joseph <62872551+jitin-joseph@users.noreply.github.com>
2020-07-20 11:20:01 +0530

    Update index.php
    enhancements in code

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 
 
+
+
 <form method="POST" action="">
+Enter the language file name
+<input name="folder_name" value="<?php echo isset($_POST['folder_name']) ? $_POST['folder_name'] : ''; ?>"><br/>
 Enter the string
 <textarea name="original_string"></textarea><br/>
 <input type="submit" name="submit"><br/>
@@ -12,23 +16,24 @@ Enter the string
 Into language file<br/><br/>
 
 <?php
-$original_string = isset($_POST['original_string'])?$_POST['original_string']:"";
+$original_string = isset($_POST['original_string']) ? $_POST['original_string'] : "";
+$folder_name = "";
+$folder_name = isset($_POST['folder_name']) ? $_POST['folder_name'] : "";
 
 $underscored = preg_replace('/\s+/', '_', $original_string);
 $lower_case = strtolower($underscored);
 
-
 //need to get in below format
 //'read_mail'              => 'Read Mail'
 
-echo "'".$lower_case."'		\t	=>		'".$original_string."',";
+echo "'" . $lower_case . "'		\t	=>		'" . $original_string . "',";
 
-?>	
+?>
 
 <br/><br/>
 Into language variable<br/><br/>
 <?php
 //need to get in this format
 //{{trans('language.dont_hav_permission')}}
-$folder_name = "mailbox";
+
 echo "{{trans('$folder_name.$lower_case')}}";
